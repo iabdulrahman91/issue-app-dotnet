c8b664b8950e4d50ca07e8ceba05a1aec92f2886
iabdulrahman91 <iabdulrahman91@users.noreply.github.com>
2023-01-24 08:21:38 +0000

    initialize unit of work pattern

---
+++ IssueDemo/IssueDemo.Application/Common/Interfaces/Persistence/IRepository.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+using IssueDemo.Application.Common.Interfaces;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    TEntity Get(int id);
+    IEnumerable<TEntity> GetAll();   
+    IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+
+    void Add(TEntity entity);
+    void AddRange(IEnumerable<TEntity> entities);
+    void Remove(TEntity entity);
+    void RemoveRange(IEnumerable<TEntity> entities);
+}
---
+++ IssueDemo/IssueDemo.Infrastructure/Persistence/Repository.cs
@@ -0,0 +1,51 @@
+using System.Linq.Expressions;
+using IssueDemo.Application.Common.Interfaces.Persistence;
+using IssueDemo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace IssueDemo.Infrastructure.Persistance;
+
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+{
+    protected readonly DbContext _dbContext;
+
+    public Repository(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public void Add(TEntity entity)
+    {
+        _dbContext.Set<TEntity>().Add(entity);
+    }
+
+    public void AddRange(IEnumerable<TEntity> entities)
+    {
+        _dbContext.Set<TEntity>().AddRange(entities);
+    }
+
+    public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+    {
+        return _dbContext.Set<TEntity>().Where(predicate);
+    }
+
+    public TEntity Get(int id)
+    {
+        return _dbContext.Set<TEntity>().Find(id);
+    }
+
+    public IEnumerable<TEntity> GetAll()
+    {
+        return _dbContext.Set<TEntity>().ToList();
+    }
+
+    public void Remove(TEntity entity)
+    {
+        _dbContext.Set<TEntity>().Remove(entity);
+    }
+
+    public void RemoveRange(IEnumerable<TEntity> entities)
+    {
+        _dbContext.Set<TEntity>().RemoveRange(entities);
+    }
+}
